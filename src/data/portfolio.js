0726935265795b1d53b1f84704847b9fe6607bd3
Aditya Balakrishnan <adihex@tutanota.com>
2024-08-25 23:06:38 +0530

    update:portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,14 +1,14 @@
 export default [
   {
     title: 'Sorting Algorithm Visualizer',
-    imgUrl: '/assets/sorting_visualizer-min.png',
+    imgUrl: 'assets/sorting_visualizer-min.png',
     stack: ['HTML', 'CSS', 'JS'],
     link: 'https://adihex.github.io/Sorting-Visualiser/',
   },
   {
     title: 'Chatty-Pete',
-    imgUrl: '/assets/chatty-pete.png',
-    stack: ['Next.js', 'Tailwind', 'MongoDB'],
+    imgUrl: 'assets/chatty-pete.png',
+    stack: ['Next.js', 'Tailwind', 'MonoDB'],
     link: 'https://github.com/adihex/chatty-pete',
   },
 ];
