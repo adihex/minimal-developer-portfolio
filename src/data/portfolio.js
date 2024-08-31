617fefd1c5e03adddba0dbe64d4d5724202f2dd0
Aditya Balakrishnan <adihex@tutanota.com>
2024-08-31 12:08:14 +0530

    Updated data again

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -15,5 +15,12 @@ export default [
       title: 'Food Ordering Website',
       imgUrl: 'assets/food_website-min.png',
       stack: ['React.js', 'Bootstrap', 'Express.js', 'MySQL'],
+      link: 'https://github.com/adihex/foodorderingwebsite',
     },
+    {
+      title: 'Minimal Developer Portfolio',
+      imgUrl: 'assets/minimal_portfolio.png',
+      stack: ['React.js','Tailwind CSS','GitHub Actions']
+      link: 'https://github.com/adihex/minimal-developer-portfolio'
+    }
 ];
